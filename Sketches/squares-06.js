83dd3dae28cd3c3202d03f799759cd3b4e4e389e
Rick Turner <rick.j.turner@gmail.com>
2022-07-07 17:52:25 +0100

    Create squares-06.js

---
+++ Sketches/squares-06.js
@@ -0,0 +1,135 @@
+const canvasSketch = require('canvas-sketch');
+const random = require('canvas-sketch-util/random');
+const math = require('canvas-sketch-util/math');
+const tweakpane = require('tweakpane');
+
+const settings = {
+    dimensions: [1080, 1080],
+    animate: true
+};
+
+const params = {
+    cols: 10,
+    rows: 10,
+    scaleMin: 1,
+    scaleMax: 10,
+    circleLineScale: 2,
+    radius: 16,
+    gradient: 60,
+    rectSize: 1,
+    speed: 10,
+    freq: 0.001,
+    amp: 0.2,
+    frame: 0,
+    animate: true,
+};
+
+const colorA = randomColor();
+const colorB = randomColor();
+
+const sketch = () => {
+    return ({context, width, height, frame}) => {
+        context.fillStyle = 'white';
+        context.fillRect(0, 0, width, height);
+
+
+        const cols = params.cols;
+        const rows = params.rows;
+        const numCells = cols * rows;
+
+        const gridw = width * 0.8;
+        const gridh = height * 0.8;
+        const cellw = gridw / cols;
+        const cellh = gridh / rows;
+        const margx = (width - gridw) * 0.5;
+        const margy = (height - gridh) * 0.5;
+
+        for (let i = 0; i < numCells; i++) {
+            const col = i % cols;
+            const row = Math.floor(i / cols);
+
+            const x = col * cellw;
+            const y = row * cellh;
+            const w = cellw * 0.8;
+            const h = cellh * 0.8;
+
+            const f = params.animate ? frame : params.frame;
+
+            const n = random.noise3D(x, y, f * params.speed, params.freq);
+
+            const angle = n * Math.PI * params.amp;
+
+            const scale = math.mapRange(n, -1, 1, params.scaleMin, params.scaleMax);
+
+            context.save();
+            context.translate(x, y);
+            context.translate(margx, margy);
+            context.rotate(angle);
+
+            context.lineWidth = scale;
+
+            var gradient = context.createLinearGradient(0, 0, params.gradient, 0);
+            gradient.addColorStop("0", colorA);
+            gradient.addColorStop("1.0", colorB);
+            context.strokeStyle = gradient;
+
+
+            context.beginPath();
+            context.rect(0, 0, w * params.rectSize, h * params.rectSize);
+            context.stroke();
+
+            context.beginPath();
+            let a = w * 0.5;
+            let b = h * 0.5;
+            context.arc(a, b, params.radius, 0, 2 * Math.PI);
+            context.strokeStyle = gradient;
+            context.lineWidth = params.circleLineScale;
+            context.stroke();
+
+            context.restore();
+
+        }
+
+
+    };
+};
+
+const createPane = () => {
+    const pane = new tweakpane.Pane();
+    let folder;
+
+    folder = pane.addFolder({title: 'Grid'});
+    folder.addInput(params, 'cols', {min: 2, max: 50, step: 1});
+    folder.addInput(params, 'rows', {min: 2, max: 50, step: 1});
+    folder.addInput(params, 'speed', {min: 1, max: 50, step: 1});
+
+    folder = pane.addFolder({title: 'ShapeControl'});
+    folder.addInput(params, 'scaleMin', {min: 1, max: 100});
+    folder.addInput(params, 'scaleMax', {min: 1, max: 100});
+    folder.addInput(params, 'gradient', {min: 1, max: 180});
+    folder.addInput(params, 'circleLineScale', {min: 1, max: 100});
+    folder.addInput(params, 'radius', {min: 1, max: 50});
+    folder.addInput(params, 'rectSize', {min: 0.1, max: 2});
+
+    folder = pane.addFolder({title: 'Noise'});
+    folder.addInput(params, 'freq', {min: -0.01, max: 0.01});
+    folder.addInput(params, 'amp', {min: 0, max: 1});
+    folder.addInput(params, 'animate');
+    // folder.addInput(params, 'rotate');
+    folder.addInput(params, 'frame', {min: 0, max: 999});
+};
+
+/*function selectRandom(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+}*/
+
+function randomColor() {
+    return "#" + ('00000' + (Math.random() * (1 << 24) | 0).toString(16)).slice(-6);
+}
+
+/*function compColor(color) {
+    return 0xffffff ^ color;
+}*/
+
+createPane();
+canvasSketch(sketch, settings);
