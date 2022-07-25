bc8d86487e35e64c63c5a363b4e92c0f9fb7553a
Rick Turner <dropxlr@gmail.com>
2022-07-25 21:20:19 +0100

    Add Glyphs to Sketch-06
    Added Glyphs to Sketch-06, ready for customisation

--- Sketches/sketch-06_R.js
+++ Sketches/sketch-06_R.js
@@ -1,5 +1,4 @@
 const canvasSketch = require('canvas-sketch');
-const random = require('canvas-sketch-util/random');
 
 const settings = {
     dimensions: [1080, 1080]
@@ -9,7 +8,7 @@ let manager;
 
 let text = 'R';
 let fontSize = 1200;
-let fontFamily = 'Serif';
+let fontFamily = 'Sans-Serif';
 
 const typeCanvas = document.createElement('canvas');
 const typeContext = typeCanvas.getContext('2d');
@@ -27,7 +26,7 @@ const sketch = ({context, width, height}) => {
         typeContext.fillStyle = 'black';
         typeContext.fillRect(0, 0, cols, rows);
 
-        fontSize = cols * 1.2;
+        fontSize = cols;
 
         typeContext.fillStyle = 'white';
         // context.font = fontSize + 'px ' + fontFamily; Best to use a Template Literal as follows
@@ -54,14 +53,9 @@ const sketch = ({context, width, height}) => {
         typeContext.restore();
 
         const typeData = typeContext.getImageData(0, 0, cols, rows).data;
+        console.log(typeData);
 
-        context.fillStyle = 'black';
-        context.fillRect(0, 0, width, height);
-
-        context.textBaseline = 'middle';
-        context.textAlign = 'center';
-
-        // context.drawImage(typeCanvas, 0, 0);
+        context.drawImage(typeCanvas, 0, 0);
 
         for (let i = 0; i < numCells; i++) {
             const col = i % cols;
@@ -75,12 +69,7 @@ const sketch = ({context, width, height}) => {
             const b = typeData[i * 4 + 2];
             const a = typeData[i * 4 + 3];
 
-            const glyph = getGlyph(r);
-
-            context.font = `${cell * 2}px ${fontFamily}`;
-            if (Math.random() < 0.1) context.font = `${cell * 6}px ${fontFamily}`;
-
-            context.fillStyle = 'white';
+            context.fillStyle = `rgb(${r}, ${g}, ${b})`;
 
             context.save();
             context.translate(x, y);
@@ -88,24 +77,14 @@ const sketch = ({context, width, height}) => {
 
             // context.fillRect(0, 0, cell, cell);
 
-            context.fillText(glyph, 0, 0);
+            // creates canvas with small font, liked so kept to play with
+            context.fillText(text, 0, 0);
 
             context.restore();
 
         }
     };
 };
-
-const getGlyph = (v) => {
-    if (v < 50) return '';
-    if (v < 100) return '.';
-    if (v < 150) return '-';
-    if (v < 200) return '+';
-
-    const glyphs = '_=/'.split('');
-
-    return random.pick(glyphs);
-}
 
 const onKeyUp = (e) => {
     text = e.key.toUpperCase();
